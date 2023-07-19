919655a7eb92b0b24671da19a2bc24e9bc0d2c13
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-20 00:22:28 +0200

    app.jsx updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,21 @@
-import React, { useState, useEffect } from "react";
-import s from "./App.module.css";
-import ContactForm from "components/ContactForm/ContactForm";
-import ContactList from "components/ContactList/ContactList";
-import Filter from "components/Filter/Filter";
-import { v4 as uuidv4 } from "uuid";
+import React from 'react';
+import s from './App.module.css';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
-
-  const [filter, setFilter] = useState("");
-
-  useEffect(() => {
-    const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
-    parsedContacts && setContacts(parsedContacts);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
-
-  const handleAddContact = (data) => {
-    const newContact = { ...data, id: uuidv4() };
-
-    if (
-      contacts
-        .map((contact) => contact.name.toLowerCase())
-        .includes(data.name.toLowerCase())
-    ) {
-      alert(`Contact "${data.name}" already exists`);
-    } else {
-      setContacts((prevContacts) => [newContact, ...prevContacts]);
-    }
-  };
-
-  const handleFilter = (event) => {
-    const { value } = event.currentTarget;
-    setFilter(value);
-  };
-
-  const getMatchingContacts = () => {
-    const optimizedFilter = filter.toLowerCase();
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(optimizedFilter)
-    );
-  };
-
-  const deleteContact = (contactId) => {
-    setContacts((prevContacts) =>
-      prevContacts.filter((contact) => contact.id !== contactId)
-    );
-  };
-
-  const filteredContacts = getMatchingContacts();
-
   return (
     <div className={s.container}>
       <section title="Phonebook" className={s.section}>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={handleAddContact} />
+        <ContactForm />
       </section>
 
       <section title="Contacts" className={s.section}>
         <h2>Contacts</h2>
-        <Filter value={filter} onChange={handleFilter} />
-        <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
+        <Filter />
+        <ContactList />
       </section>
     </div>
   );
